b9ac58d1c428521c8d9f15a074265f1d71aed614
Malik H <malik.h@webdevpro.net>
2025-04-08 21:02:25 +0200

    docker build

--- script.sh
+++ script.sh
@@ -17,7 +17,7 @@ echo "EXPOSE 5050" >> tempdir/Dockerfile                              #(6)
 echo "CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile    #(7)
 
 cd tempdir                                                            #(8)
-# docker build -t sampleapp .                                           #(8)
+docker build -t sampleapp .                                           #(8)
 # 
 # docker run -t -d -p 5050:5050 --name samplerunning sampleapp          #(9)
 # docker ps -a  
